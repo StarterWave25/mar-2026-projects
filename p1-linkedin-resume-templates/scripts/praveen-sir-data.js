4a413a36ec2ef14af46ab151ed86840d0e070b4e
Your Name <you@example.com>
2026-03-07 10:49:59 +0530

    Data Added

---
+++ p1-linkedin-resume-templates/scripts/praveen-sir-data.js
@@ -0,0 +1,12 @@
+const userData = {
+    fullName: 'Praveen Gubbala',
+    tagline: 'Fullstack Consultant Trainer',
+    place: 'Hyderabad, Telangana, India',
+    summary: 'Dedicated training professional that strives to present cutting edge technology in a manner that neophytes and craftsmen can garner information. Committed to providingindividualized attention to meet pre-set criteria for completing the task at hand. An expert in performance based training that fosters the proper solution for the concern at hand.',
+    experience: [
+        { role: 'Training Consultant', company: 'ThoughtWorks', period: 'Dec 2025 - Present' },
+        { role: 'Founder', company: 'Edupoly', period: 'Jan 2021 - Present' },
+        { role: 'Corporate Trainer', company: 'upGrad', period: 'May 2022 - Present' },
+    ],
+    contact: { phone: 7875381293, email: 'praveengclasses@gmail.com' }
+}
