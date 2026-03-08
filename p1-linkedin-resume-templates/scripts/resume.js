79bb9a08150d2bfa6760270dc1cd3e2a8506ed26
prudvi <patnamprudvinath2006@gmail.com>
2026-03-08 21:01:16 +0530

    Added Functionality to edit-resume (Download - resume, resume templates rendering)

---
+++ p1-linkedin-resume-templates/scripts/resume.js
@@ -0,0 +1,36 @@
+let resumeId = 'tpl-1';
+
+const resumeContainer = document.querySelector('.resume-section');
+const resumeTemplatesContainer = document.querySelector('.resume-templates-section');
+
+const renderResume = (resumeId) => {
+  let resumeHTML = document.querySelector(`#${resumeId}`).innerHTML;
+  resumeContainer.innerHTML = resumeHTML;
+}
+
+renderResume(resumeId);
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  resumeContainer.addEventListener('click', () => {
+    if (!document.fullscreenElement) {
+      resumeContainer.requestFullscreen();
+    } else {
+      document.exitFullscreen();
+    }
+  });
+
+  const downloadBtn = document.querySelector('.download-btn');
+
+  downloadBtn.addEventListener('click', () => {
+    html2pdf().from(resumeContainer.innerHTML).save();
+  });
+
+  resumeTemplatesContainer.addEventListener('click', (ev) => {
+    const card = ev.target.closest('.resume-card');
+
+    if (!card) return;
+    renderResume(card.id);
+  });
+
+});
